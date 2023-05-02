8f6cae77a39b94c96f56a70051d2140d4a4fb838
afratetlay <afratetlay@gmail.com>
2023-05-02 16:02:41 +0200

    adding style component to project

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,13 +1,16 @@
-import React from 'react';
+import React from "react";
+import styled from "styled-components";
 
-import './Button.css';
+import "./Button.css";
 
-const Button = props => {
-  return (
-    <button type={props.type} className="button" onClick={props.onClick}>
-      {props.children}
-    </button>
-  );
-};
+const Button = styled.button``;
+
+// const Button = props => {
+//   return (
+//     <button type={props.type} className="button" onClick={props.onClick}>
+//       {props.children}
+//     </button>
+//   );
+// };
 
 export default Button;
